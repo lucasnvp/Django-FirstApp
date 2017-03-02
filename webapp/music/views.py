bc36d5a558709f1ad7c9ea29890df732289b58eb
Lucas Visser <lucasnvp@gmail.com>
2017-03-02 12:14:55 -0300

    Namespace and HTTp 404 Shortcut

--- webapp/music/views.py
+++ webapp/music/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from .models import Album
 
@@ -7,8 +7,6 @@ def index(request):
     return render(request, 'music/index.html', {'all_albums': all_albums})
 
 def detail(request, album_id):
-    try:
-        album = Album.objects.get(pk=album_id)
-    except Album.DoesNotExist:
-        raise Http404("Album does not exist")
+    #album = Album.objects.get(pk=album_id)
+    album = get_object_or_404(Album, pk=album_id)
     return render(request, 'music/detail.html', {'album': album})
